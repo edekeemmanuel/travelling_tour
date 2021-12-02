75615ea2494386f6ce5cc66144ac51c59cf5faaf
Edeke Emmanuel <edekeemmanuel55@gmail.com>
2021-12-02 14:33:09 +0100

    Update Tours.js

--- src/Tours.js
+++ src/Tours.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import End from "./End";
+import url from "./url";
 import "./index.css";
 
-const url = "https://course-api.com/react-tours-project";
 
 const Tours = () => {
   const [value, setValue] = useState([]);
